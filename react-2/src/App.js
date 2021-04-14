7ca6f88ecd33503f6995678f63f386f893aa0c2b
Edwin Vazquez <edwinvazquezcal12@gmail.com>
2021-04-13 19:39:21 -0500

    ventana modal con react

--- react-2/src/App.js
+++ react-2/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Nav from "./componentes/Nav";
+
+import "./App.css";
+import Pagina from "./Pagina";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Nav />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={(props) => (
+            <Pagina {...props} titulo="Mascotas" entidad="mascotas" />
+          )}
+        />
+        <Route
+          path="/veterinarias"
+          component={(props) => (
+            <Pagina {...props} titulo="Veterinari@s" entidad="veterinarias" />
+          )}
+        />
+        <Route
+          path="/duenos"
+          component={(props) => (
+            <Pagina {...props} titulo="Dueñ@s" entidad="duenos" />
+          )}
+        />
+        <Route
+          path="/consultas"
+          component={(props) => (
+            <Pagina {...props} titulo="Consultas" entidad="consultas" />
+          )}
+        />
+      </Switch>
     </div>
   );
 }
